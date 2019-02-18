c1f85a92208110138b9cfde91e753339581840ac
David Andersson <David Andersson>
2019-02-18 10:02:04 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const data = [
+	{
+		art: 'kakadua'
+	},
+	häst
+]
+
+// () - parentes
+// {} - blockparentes, curly braces
+// [] - square brackets, hakparentes
+// , - kommatecken
+// : - kolon
+// ; - semikolon
+// / - snestreck, forward slash
+// \ - backslash
+// '' - apostrof, strängar i JS
+// "" - citattecken, quote, HTML, CSS, JS
+// `` - grav, backticks, JS
+// ´´ - accent (används ej i JavaScript)
+// $ - dollartecken
+
+//
